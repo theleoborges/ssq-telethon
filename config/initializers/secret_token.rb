48f00d52c1a7600de3ccf310e70ad54ccb29a7d6
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-06 17:08:55 +1100

    initial rails 3 skeleton app with rails-admin and devise included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SsqTelethon::Application.config.secret_token = '83ce0200c10483f51f706e31f25655889e4662ac24ccef27656d16d204e0e848635b6251297876293f485333914609bb00a14f2cf88c7b69408c9d8cafdc247b'
