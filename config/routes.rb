32c083ab72b143ff6a015538a3a439d1e8105792
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-07 10:26:36 +1100

    added main page and initial CSS copied from the real system. customising now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SsqTelethon::Application.routes.draw do
+  get "donations/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ SsqTelethon::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "donations#index"
 
   # See how all your routes lay out with "rake routes"
 
