ae5cbd2f6d45e96a79b8d98ef54f32a82ab806e4
Ben Barnard <barnardb@gmail.com>
2011-01-07 22:45:25 +1100

    prevent the gateway url builder test from breaking other tests when it fails

--- test/unit/gateway_url_builder_test.rb
+++ test/unit/gateway_url_builder_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class GatewayUrlBuilderTest < ActiveSupport::TestCase
   test "something interesting" do
-    ParamsHasher.any_instance.stubs(:to_hash).returns('84f80a3ae85bbc0ad0b7cf957352569b')    
-    builder = GatewayUrlBuilder.new("txn1", "order1", 100)
+    ParamsHasher.any_instance.stubs(:to_hash).returns('84f80a3ae85bbc0ad0b7cf957352569b')
+    begin
+      builder = GatewayUrlBuilder.new("txn1", "order1", 100)
     
-    expected_url = "https://gateway.com/vpcpay?vpc_Version=1&vpc_Locale=en&vpc_Command=pay&vpc_Merchant=merchant&vpc_AccessCode=access_code&vpc_ReturnURL=http://localhost:3000/gateway_callback&vpc_MerchTxnRef=txn1&vpc_OrderInfo=order1&vpc_Amount=100&vpc_SecureHash=84f80a3ae85bbc0ad0b7cf957352569b"
-    assert_equal expected_url, builder.to_url
-    
-    ParamsHasher.any_instance.unstub(:to_hash)
+      expected_url = "https://gateway.com/vpcpay?vpc_Version=1&vpc_Locale=en&vpc_Command=pay&vpc_Merchant=merchant&vpc_AccessCode=access_code&vpc_ReturnURL=http://localhost:3000/gateway_callback&vpc_MerchTxnRef=txn1&vpc_OrderInfo=order1&vpc_Amount=100&vpc_SecureHash=84f80a3ae85bbc0ad0b7cf957352569b"
+      assert_equal expected_url, builder.to_url
+    ensure
+      ParamsHasher.any_instance.unstub(:to_hash)
+    end
   end
 end
