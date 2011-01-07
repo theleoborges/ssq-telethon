91fedd3813b0bd25633035236879c12eca2e0a96
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-08 00:02:10 +1100

    Added mail support and template

---
+++ app/mailers/donation_mailer.rb
@@ -0,0 +1,10 @@
+class DonationMailer < ActionMailer::Base
+  default :from => "osidt@smartservice.qld.gov.au"
+  
+  def donation_confirmation(donation)
+    @donation = donation
+    mail(:to => "#{donation.customer.given_name} #{donation.customer.family_name} <#{donation.customer.email}>", 
+      :subject => "Smart Service Queensland - Receipt: #{donation.order_info}")
+  end
+  
+end
---
+++ test/functional/donation_mailer_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class DonationMailerTest < ActionMailer::TestCase
+  # replace this with your real tests
+  test "the truth" do
+    assert true
+  end
+end
