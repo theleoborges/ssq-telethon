199dfa6b1d7bdbc4c496811ddc2df0a8c31e0d73
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-07 14:37:00 +1100

    fixed the url builder. Removed hard coded uri

--- app/models/gateway_url_builder.rb
+++ app/models/gateway_url_builder.rb
@@ -1,3 +1,5 @@
+require 'params_hasher'
+
 class GatewayUrlBuilder
   # transaction_reference - this is a unique number that identifies this transaction between the app and the gateway
   # order_info - this is the same thing, but identifies the transaction between the app and the user. Not sure if it can be the same
@@ -21,7 +23,7 @@ class GatewayUrlBuilder
     }
     secure_hash = ParamsHasher.new.to_hash(params)
     
-    gateway_uri = "https://gateway.com/vpcpay?vpc_Version=1&vpc_Locale=en&vpc_Command=pay&"
+    gateway_uri = "#{AppConstants.gateway_payment_uri}?vpc_Version=1&vpc_Locale=en&vpc_Command=pay&"
     gateway_uri << "vpc_AccessCode=#{AppConstants.gateway_access_code}&vpc_MerchTxnRef=#{transaction_reference}&"
     gateway_uri << "vpc_Merchant=#{AppConstants.merchant_id}&vpc_OrderInfo=#{order_info}&vpc_Amount=#{amount}&"
     gateway_uri << "vpc_ReturnURL=#{AppConstants.gateway_callback_uri}&vpc_SecureHash=#{secure_hash}"
