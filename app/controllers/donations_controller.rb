fc6b912e267bbd7af00adef56d91e6fc3730d8b0
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-07 18:03:46 +1100

    customised callback page. it now shows a simple success/error

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,4 +1,13 @@
 class DonationsController < ApplicationController
+  ERROR_MESSAGES = {
+    "0"	=> "Transaction approved",
+    "1" => "Transaction could not be processed",
+    "E" => "Transaction declined - contact issuing bank",
+    "2" => "Transaction declined - contact issuing bank",
+    "3"	=> "No reply from Processing Host 4	Card has expired",
+    "5"	=> "Insufficient credit"
+  }
+  
   def create
     transaction_reference = UUID.new.generate(:compact).to_s
     url_builder = GatewayUrlBuilder.new transaction_reference, transaction_reference, params[:amount]
@@ -6,7 +15,8 @@ class DonationsController < ApplicationController
   end
   
   def callback
-    @request = request
+    @return_code = params["vpc_TxnResponseCode"].to_s
+    @error_msg = ERROR_MESSAGES[@return_code]
   end
 
 end
