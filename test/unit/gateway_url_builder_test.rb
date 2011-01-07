71b220f5389a24c89910dc11b07583d85def2a8b
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-07 17:10:00 +1100

    added new relic rpm and extracted common snippets to app layout

--- test/unit/gateway_url_builder_test.rb
+++ test/unit/gateway_url_builder_test.rb
@@ -5,7 +5,7 @@ class GatewayUrlBuilderTest < ActiveSupport::TestCase
     ParamsHasher.any_instance.stubs(:to_hash).returns('84f80a3ae85bbc0ad0b7cf957352569b')    
     builder = GatewayUrlBuilder.new("txn1", "order1", 100)
     
-    expected_url = "https://gateway.com/vpcpay?vpc_Command=pay&vpc_OrderInfo=order1&vpc_AccessCode=access_code&vpc_Amount=100&vpc_ReturnURL=http://localhost:3000/gateway_callback&vpc_MerchTxnRef=txn1&vpc_Version=1&vpc_Merchant=merchant&vpc_Locale=en&vpc_SecureHash=84f80a3ae85bbc0ad0b7cf957352569b"
+    expected_url = "https://gateway.com/vpcpay?vpc_Version=1&vpc_Merchant=merchant&vpc_Locale=en&vpc_Command=pay&vpc_OrderInfo=order1&vpc_AccessCode=access_code&vpc_Amount=100&vpc_ReturnURL=http://localhost:3000/gateway_callback&vpc_MerchTxnRef=txn1&vpc_SecureHash=84f80a3ae85bbc0ad0b7cf957352569b"
     assert_equal expected_url, builder.to_url
     
     ParamsHasher.any_instance.unstub(:to_hash)
