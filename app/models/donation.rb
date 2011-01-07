4d3d184ef68fc1874554695fceb33e01156fe39d
Ben Barnard <barnardb@gmail.com>
2011-01-08 02:38:09 +1100

    make sure the amount is at least $1 on the server side

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :customer
-  validates_presence_of :amount, :transaction_reference, :order_info
+  validates_presence_of :transaction_reference, :order_info
+  validates_numericality_of :amount, :greater_than_or_equal_to => 1.00
 
   after_initialize do
     self.generate_identifiers! if new_record?
