484e88e0017ca9e2355eff1e3d2b1b75fc5d4e56
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-07 16:47:13 +1100

    fixed nasty bug in the hasher

--- lib/params_hasher.rb
+++ lib/params_hasher.rb
@@ -3,11 +3,10 @@ require 'digest'
 class ParamsHasher
   def to_hash(params = {})
     return if params.empty?
-    secret = params.delete(:secret)
+    secret = params.delete(:secret).clone
     string = params.keys.sort.inject(secret) { |acc, key|
       acc << params[key].to_s
     }
-    puts string
     Digest::MD5.new.hexdigest(string)
   end
 end
