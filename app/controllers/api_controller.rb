94e208d1fd40758af4ce16e18b80ffeab4212f45
Phillip Calcado <pcalcado@gmail.com>
2011-01-08 12:04:13 +1100

    added where to sum

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def totals
-    data = {'total' => Donation.sum('amount'), 'donations' => Donation.count}
+    data = {'total' => Donation.sum('amount', :conditions => ["return_code = ?", '0']), 'donations' => Donation.count}
     render :json => data.to_json
   end
 end
