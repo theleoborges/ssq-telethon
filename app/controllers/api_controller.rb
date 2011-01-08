75fd9323f36b52966cd35bc493f91b8e31d9a849
Phillip Calcado <pcalcado@gmail.com>
2011-01-08 17:35:02 +1100

    fixed # of donations

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,8 @@
 class ApiController < ApplicationController
   def totals
     total = Donation.sum('amount', :conditions => ["return_code = ?", '0']).to_s
-    data = {'total' => total, 'donations' => Donation.count}
+    donation = Donation.where("return_code = '0'").count
+    data = {'total' => total, 'donations' => donation}
     render :json => data.to_json
   end
 end
