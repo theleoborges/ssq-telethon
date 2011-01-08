f0148d6fe43a4305f5e19c51c7886d55652fc09a
Leonardo Borges <leonardoborges.rj@gmail.com>
2011-01-08 19:54:41 +1100

    only set smtp in dev env

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,16 @@
 require 'development_mail_interceptor'
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "leonardoborges.com",
-  :user_name            => "do-not-reply@leonardoborges.com",
-  :password             => "labemoldelivery",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+
+if Rails.env.development?
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "leonardoborges.com",
+    :user_name            => "do-not-reply@leonardoborges.com",
+    :password             => "labemoldelivery",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+end
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
