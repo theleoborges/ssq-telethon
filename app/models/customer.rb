df3f7672c1e634e5eebfa22869bda9d1cfc0e4d6
James Ottaway <james@ottaway.mp>
2011-01-19 14:44:19 +1000

    Added a few basic scopes to models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ActiveRecord::Base
+  scope :snail_mail_receipt, where(:wants_receipt_by_snail_mail => true)
+  scope :email_receipt, where(:wants_receipt_by_email => true)
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,9 +1,10 @@
 class Donation < ActiveRecord::Base
   belongs_to :customer
   validates_numericality_of :amount, :greater_than_or_equal_to => 1.00, :message => "invalid number - use one or two decimal places, or no decimal point"
-  
+
+  scope :paid, where(:return_code => '0')
+
   def display_amount
     sprintf "%.2f", self.amount.to_f
   end
-  
 end
