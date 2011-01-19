af220137dd0470397de9032ec91eb12eb4c96901
James Ottaway <james@ottaway.mp>
2011-01-19 15:42:08 +1000

    Overrode 'to_s' on Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ActiveRecord::Base
   scope :snail_mail_receipt, where(:wants_receipt_by_snail_mail => true)
   scope :email_receipt, where(:wants_receipt_by_email => true)
+  
+  def to_s
+    title + " " + given_name + " " + family_name
+  end
 end
