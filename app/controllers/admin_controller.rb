1ddbe0779b9af9fcf1f31ff4336c1136483dfff5
James Ottaway <james@ottaway.mp>
2011-01-20 15:47:16 +1000

    Made text searches in receipt reissue action handle case-insensitivity as well as doing a LIKE instead of checking equality

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -22,9 +22,9 @@ class AdminController < ApplicationController
       
       @donations = @donations.where(:id => @receipt_search.receipt_number) unless @receipt_search.receipt_number == ""
       @donations = @donations.where(:amount => @receipt_search.amount) unless @receipt_search.amount == ""
-      @donations = @donations.where("customers.given_name = ?", @receipt_search.given_name) unless @receipt_search.given_name == ""
-      @donations = @donations.where("customers.family_name = ?", @receipt_search.family_name) unless @receipt_search.family_name == ""
-      @donations = @donations.where("customers.organisation_name = ?", @receipt_search.organisation_name) unless @receipt_search.organisation_name == ""
+      @donations = @donations.where("customers.given_name ILIKE ?", "%" + @receipt_search.given_name + "%") unless @receipt_search.given_name == ""
+      @donations = @donations.where("customers.family_name ILIKE ?", "%" + @receipt_search.family_name + "%") unless @receipt_search.family_name == ""
+      @donations = @donations.where("customers.organisation_name ILIKE ?", "%" + @receipt_search.organisation_name + "%") unless @receipt_search.organisation_name == ""
       @donations = @donations.where("customers.email = ?", @receipt_search.email_address) unless @receipt_search.email_address == ""
       @donations = @donations.where("donations.updated_at >= ?", date - 10.hours) unless date.nil?
       @donations = @donations.where("donations.updated_at <= ?", date + 1.day - 10.hours) unless date.nil?
