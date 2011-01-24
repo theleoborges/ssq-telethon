479ef8569d8398782ea87fbc5b4cdd72d7db32ea
James Ottaway <james@ottaway.mp>
2011-01-24 23:05:18 +1000

    Added validation to Customer model and fixed DonationController to use it. Fixed HTML to link up to validation errors.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ActiveRecord::Base
+  validates_presence_of :email, :if => :wants_receipt_by_email
+  validates_presence_of :address_line_1, :city, :state, :postalcode, :country, :if => :wants_receipt_by_snail_mail
 end
