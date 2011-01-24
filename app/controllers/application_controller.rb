94f87c49fe1159959053054133af031d5a7ea2fc
James Ottaway <james@ottaway.mp>
2011-01-24 18:09:35 +1000

    Added SSL redirect in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
-#  protect_from_forgery
-  
-  before_filter :clear_etag
+  CANONICAL_HOST = 'telethon.smartservice.qld.gov.au'
+
+  before_filter :clear_etag, :redirect_to_ssl
   
   def clear_etag
     response.etag = nil
+  end
+  
+  def redirect_to_ssl
+    return if ENV["NO_SSL"]
+    return unless Rails.env.production?
+    return if request.ssl? && request.host == CANONICAL_HOST
+    redirect_to 'https://' + CANONICAL_HOST
   end
 end
