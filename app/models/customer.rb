98b0c8d901c5da6b84266a9754f5d587f3d3fffd
James Ottaway <james@ottaway.mp>
2011-01-27 11:19:45 +1000

    Added email format and email confirmation validation to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ActiveRecord::Base
   validates_presence_of :email, :if => :wants_receipt_by_email
   validates_presence_of :address_line_1, :city, :state, :postalcode, :country, :if => :wants_receipt_by_snail_mail
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :allow_blank => true
+  validates_confirmation_of :email
 end
