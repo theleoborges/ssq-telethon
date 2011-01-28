3fa3b6c8df8b34e93d55cdd22838c9904dd8f85b
James Ottaway <james@ottaway.mp>
2011-01-28 12:26:23 +1000

    Resolved strange datetime formatting which only showed the time

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,3 +1,3 @@
 Date::DATE_FORMATS[:default] = "%d/%m/%Y"
-Time::DATE_FORMATS[:default] = "%H:%M:%S"
+Time::DATE_FORMATS[:default] = "%d/%m/%Y %H:%M:%S"
 DateTime::DATE_FORMATS[:default] = "%d/%m/%Y %H:%M:%S"
