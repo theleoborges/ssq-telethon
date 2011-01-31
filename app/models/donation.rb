8ff17d7640c1013dd6be63b010a561526d8eff0b
peter_dee <ssqosidt@gmail.com>
2011-01-31 15:46:41 +1000

    changed our sites max donation limit to 20mil. Since CBA is maxintval.

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,7 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :customer
   validates_numericality_of :amount, :greater_than_or_equal_to => 1.00, :message => "invalid number - use one or two decimal places, or no decimal point"
-  validates_numericality_of :amount, :less_than => 100000000.00, :message => "invalid number - maximum valid amount is $99,999,999.99"
+  validates_numericality_of :amount, :less_than_or_equal_to => 20000000.00, :message => "invalid number - the maximum allowable donation is $20,000,000.00"
   validates_associated :customer
 
   scope :paid, where(:return_code => '0')
