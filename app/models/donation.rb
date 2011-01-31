c451d673b0fe2f8264aed87d639bf05762f27745
peter_dee <ssqosidt@gmail.com>
2011-01-31 15:23:45 +1000

    fixed donation amount validation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,7 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :customer
-  validates_numericality_of :amount, :greater_than_or_equal_to => 1.00, :message => "invalid number - use one or two decimal places, or no decimal point",
-                                     :less_than => 100000000.00, :message => "invalid number - maximum valid amount is $99'999'999.99"
+  validates_numericality_of :amount, :greater_than_or_equal_to => 1.00, :message => "invalid number - use one or two decimal places, or no decimal point"
+  validates_numericality_of :amount, :less_than => 100000000.00, :message => "invalid number - maximum valid amount is $99,999,999.99"
   validates_associated :customer
 
   scope :paid, where(:return_code => '0')
