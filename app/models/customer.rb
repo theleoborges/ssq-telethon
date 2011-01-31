ae82a7e93260e9f099bb1f2b8ec5166cbe181114
James Ottaway <james@ottaway.mp>
2011-01-31 15:56:28 +1000

    added remaining content to receipt csv

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,9 +6,16 @@ class Customer < ActiveRecord::Base
 
   scope :snail_mail_receipt, where(:wants_receipt_by_snail_mail => true)
   scope :email_receipt, where(:wants_receipt_by_email => true)
-  
+
   def to_s
-    title + " " + given_name + " " + family_name
+    name
+  end
+
+  def name
+    [title,given_name,family_name].select{|e| e and !e.empty?}.join(' ')
+  end
+
+  def address
+    [address_line_1, address_line_2, address_line_3, city, state, postalcode, country].select{|e| e and !e.empty?}.join(' ')
   end
 end
-
