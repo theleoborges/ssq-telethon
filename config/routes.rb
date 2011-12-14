e8ee1f321687e89e8c719cbef26ae29a7b769cff
James Ottaway <james@ottaway.mp>
2011-12-14 16:22:09 +1000

    Can now specify an optional relative root URL via an env variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,77 +1,79 @@
 SsqTelethon::Application.routes.draw do
-  devise_for :users
+  scope ENV['RAILS_RELATIVE_ROOT'] || '/' do
+    devise_for :users
 
-  get "api/totals"
+    get "api/totals"
 
-  get "e5f4a96ae0e556fc021037059c97a640" => "donations#e5f4a96ae0e556fc021037059c97a640"
-  get "donations/index"
-  get "donations/callback"
-  get "donations/complete/:transaction_reference" => "donations#complete", :as => :donations_complete
-  get "donations/retry"
-  post "donations" => "donations#create"
-  
-  get "admin/receipt_search"
-  post "admin/find_receipts"
-  get "admin/donation/:donation_id" => "admin#donation", :as => :admin_donation
-  post "admin/reissue_receipt" => "admin#reissue_receipt"
-  
-  get "admin/download_receipts"
-  post "admin/download_receipts_csv"
+    get "e5f4a96ae0e556fc021037059c97a640" => "donations#e5f4a96ae0e556fc021037059c97a640"
+    get "donations/index"
+    get "donations/callback"
+    get "donations/complete/:transaction_reference" => "donations#complete", :as => :donations_complete
+    get "donations/retry"
+    post "donations" => "donations#create"
+    
+    get "admin/receipt_search"
+    post "admin/find_receipts"
+    get "admin/donation/:donation_id" => "admin#donation", :as => :admin_donation
+    post "admin/reissue_receipt" => "admin#reissue_receipt"
+    
+    get "admin/download_receipts"
+    post "admin/download_receipts_csv"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+    # The priority is based upon order of creation:
+    # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+    # Sample of regular route:
+    #   match 'products/:id' => 'catalog#view'
+    # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+    # Sample of named route:
+    #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+    # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # Sample resource route (maps HTTP verbs to controller actions automatically):
+    #   resources :products
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    # Sample resource route with options:
+    #   resources :products do
+    #     member do
+    #       get 'short'
+    #       post 'toggle'
+    #     end
+    #
+    #     collection do
+    #       get 'sold'
+    #     end
+    #   end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+    # Sample resource route with sub-resources:
+    #   resources :products do
+    #     resources :comments, :sales
+    #     resource :seller
+    #   end
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+    # Sample resource route with more complex sub-resources
+    #   resources :products do
+    #     resources :comments
+    #     resources :sales do
+    #       get 'recent', :on => :collection
+    #     end
+    #   end
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # Sample resource route within a namespace:
+    #   namespace :admin do
+    #     # Directs /admin/products/* to Admin::ProductsController
+    #     # (app/controllers/admin/products_controller.rb)
+    #     resources :products
+    #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "donations#index"
+    # You can have the root of your site routed with "root"
+    # just remember to delete public/index.html.
+    root :to => "donations#index"
 
-  # See how all your routes lay out with "rake routes"
+    # See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+    # This is a legacy wild controller route that's not recommended for RESTful applications.
+    # Note: This route will make all actions in every controller accessible via GET requests.
+    # match ':controller(/:action(/:id(.:format)))'
+  end
 end
